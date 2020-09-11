a08b5e0bfc6533cfe409442e078ada2fcd183124
EdONeill1 <edward.oneill@ucdconnect.ie>
2020-09-11 16:19:53 +0100

    Calculator Still Works. Plan : Since ParseProgram is [HVal], Expr needs to be in terms of that

--- Main.hs
+++ Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import HParser
---import Expr
+import Expr
 
 import Data.Char
 import System.IO
@@ -27,40 +27,40 @@ import Text.Parsec hiding ((<|>))
 --print' :: String -> IO ()
 --print' = putStrLn
 
---flushStr :: String -> IO ()
---flushStr str = putStr str >> hFlush stdout
---
---readPrompt :: String -> IO String
---readPrompt prompt = flushStr prompt >> getLine
---
---evalString :: String -> IO String
---evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
---
---evalAndPrint :: String -> IO ()
---evalAndPrint expr = evalString expr >>= putStrLn
---
---until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
---until_ pred prompt action = do
--- result <- prompt
--- if pred result
---    then return ()
---    else action result >> until_ pred prompt action
---
---runRepl :: IO ()
---runRepl = until_ (== "quit") (readPrompt "Olivia> ") evalAndPrint
---
---main :: IO ()
---main = do
---  args <- getArgs
---  case length args of
---    0 -> runRepl
---    1 -> evalAndPrint $ args !! 0
---    otherwise -> putStrLn "Program takes only 0 or 1 arguements"
---
---readExpr :: String -> ThrowsError HVal
---readExpr input = case parse parseExpr "Olivia" input of
---   Left err -> return $ HString $ "Error: " ++ show err
---   Right val -> return $ val
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+ result <- prompt
+ if pred result
+    then return ()
+    else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Olivia> ") evalAndPrint
+
+main :: IO ()
+main = do
+ args <- getArgs
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ args !! 0
+    otherwise -> putStrLn "Program takes only 0 or 1 arguements"
+
+readExpr :: String -> ThrowsError HVal
+readExpr input = case parse parseExpr "Olivia" input of
+   Left err -> return $ HString $ "Error: " ++ show err
+   Right val -> return $ val
 
 --readExpr :: String -> String
 --readExpr input = case parse parseProgram "H" input of
@@ -73,7 +73,6 @@ parseFile file =
 		Right parsed -> return $ parsed
 
   
-main = undefined
 
 
 
