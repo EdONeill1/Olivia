acf478a09821594527cf490cdf15646415fbee8a
EdONeill1 <edward.oneill@ucdconnect.ie>
2020-09-14 12:14:39 +0100

    Recursive variable expressions are working

--- Expr.hs
+++ Expr.hs
@@ -63,14 +63,15 @@ evalDo env cond expr
   | otherwise = evalDo env cond (evalHVal env expr)
 
 
+-- May add function : evalExpr env (Expr _ _ _) op (HVal _)   .... Not sure if it's a good idea or not.
 evalExpr :: Env -> HVal -> Op -> HVal -> IOThrowsError HVal
 
 ----------- Expression Evaulation of Integers ----------
-evalExpr env (HInteger x) Add (HInteger y)  = return $ HInteger (x + y)
-evalExpr env (HInteger x) Sub (HInteger y)  = return $ HInteger (x - y)
-evalExpr env (HInteger x) Mult (HInteger y) = return $ HInteger (x * y)
-evalExpr env (HInteger x) Div (HInteger y)  = return $ HInteger (x `div` y)
-evalExpr env (HInteger x) Mod (HInteger y)  = return $ HInteger (x `mod` y)
+evalExpr env (HInteger x) Add (HInteger y)       = return $ HInteger (x + y)
+evalExpr env (HInteger x) Sub (HInteger y)       = return $ HInteger (x - y)
+evalExpr env (HInteger x) Mult (HInteger y)      = return $ HInteger (x * y)
+evalExpr env (HInteger x) Div (HInteger y)       = return $ HInteger (x `div` y)
+evalExpr env (HInteger x) Mod (HInteger y)       = return $ HInteger (x `mod` y)
 evalExpr env (HInteger x) Greater (HInteger y)   = return $ HBool (x > y)
 evalExpr env (HInteger x) GreaterEq (HInteger y) = return $ HBool (x >= y)
 evalExpr env (HInteger x) Less    (HInteger y)   = return $ HBool (x < y)
@@ -78,29 +79,21 @@ evalExpr env (HInteger x) LessEq  (HInteger y)   = return $ HBool (x <= y)
 evalExpr env (HInteger x) Equal   (HInteger y)   = return $ HBool (x == y)
 
 ----------- Expression Evaluation of Booleans ----------
-evalExpr env (HBool x) And (HBool y)             = return $ HBool (x && y)
+evalExpr env (HBool x)    And (HBool y)          = return $ HBool (x && y)
 evalExpr env (HBool x)    Or      (HBool y)      = return $ HBool (x || y)
 evalExpr env (HBool x)    Equal   (HBool y)      = return $ HBool (x == y)
 
 ----------- Expression Evaluation of Variables ---------- 
-evalExpr env (HString x)  op (HInteger y) = getVar env x >>= (\a -> evalExpr env a op (HInteger y))
-evalExpr env (HInteger x) op (HString y)  = getVar env y >>= (\a -> evalExpr env (HInteger x) op a)
-evalExpr env (HString x)  op (HString y)  = getVar env x >>= (\a -> getVar env y >>= (\b -> evalExpr env a op b))
-evalExpr env (HString x)  op (HBool y) = getVar env x >>= (\a -> evalExpr env a op (HBool y))
-evalExpr env (HBool x) op (HString y) = getVar env y >>= (\a -> evalExpr env (HBool x) op a)
+evalExpr env (HString x)  op (HInteger y)    = getVar env x >>= (\a -> evalExpr env a op (HInteger y))
+evalExpr env (HInteger x) op (HString y)     = getVar env y >>= (\a -> evalExpr env (HInteger x) op a)
+evalExpr env (HString x)  op (HString y)     = getVar env x >>= (\a -> getVar env y >>= (\b -> evalExpr env a op b))
+evalExpr env (HString x)  op (HBool y)       = getVar env x >>= (\a -> evalExpr env a op (HBool y))
+evalExpr env (HBool x)    op (HString y)     = getVar env y >>= (\a -> evalExpr env (HBool x) op a)
 
 ----------- Expression Evalation in Recursive Cases ----------
-evalExpr env (HInteger x) op (Expr a op' b) = evalExpr env (HInteger x) op (evalHValExpr env a op' b)
---evalExpr env (HBool x)    op (Expr a op' b) = evalExpr env (HBool x)    op (evalExpr env a op' b)
-
-evalHValExpr :: Env -> HVal -> Op -> HVal -> HVal
-evalHValExpr env (HInteger x) Add (HInteger y) = HInteger (x + y)
-
-
-
-
-
-
+evalExpr env (HInteger x) op (Expr a op' b) = eval env (Expr a op' b) >>= \y -> evalExpr env (HInteger x) op y
+evalExpr env (HString  x) op (Expr a op' b) = eval env (Expr a op' b) >>= \y -> getVar env x >>= \y' -> evalExpr env y op y'
+evalExpr env (HBool    x) op (Expr a op' b) = eval env (Expr a op' b) >>= \y -> evalExpr env (HBool    x) op y
 
 
 --------------------------------------------------------------
